a42d63f68e114116e9c8a95f504959ee95d4d376
mohammad shafiee <shafiee.mohamin@gmail.com>
2022-03-09 17:00:01 +0330

    base class for Agent was added

--- src/model/DoubleQNModel.py
+++ src/model/DoubleQNModel.py
@@ -1,34 +1,38 @@
+from collections import deque
+import random
+import cv2
 import gym
 import numpy as np
-from ..config import ENV_NAME
+from ..config import BATCH_SIZE, DISCOUNT_FACTOR, ENV_NAME, EPSILON, EPISODES, LRATE, MINIMUM_EXPERIENCE_MEMORY
 from .LayersWrapper import load_default_model
 from tensorflow import keras
-import cv2
 
 
-class DoubleQNModel:
-    def __init__(self):
-        self.targetModel = None
-        self.workerModel = None
-        self.env = gym.make(ENV_NAME)
-        self.input_shape  = self.env.observation_space.shape # input frame size
-        self.output_shape = self.env.action_space.n # actions count
-        print(self.input_shape, self.output_shape)
-        
+class Agent:
+    def __init__(self, env, episodes, policy):
+        self.policy = policy
+        self.episodes = episodes
+        self.env = env
+        self.input_shape = self.env.observation_space.shape  # input frame size
+        self.output_shape = self.env.action_space.n  # actions count
     
-    def createModel(self):
-        assert self.targetModel is not None
-        self.workerModel = keras.models.clone_model(self.targetModel)
-        
+    def updateParameters(self):
+        raise NotImplementedError
     
+    def train(self):
+        raise NotImplementedError
+
+
+class DoubleQNModel(Agent):
+    def __init__(self, env, episodes, model, policy):
+        super().__init__(env, episodes, policy)
+        self.targetModel = keras.models.clone_model(model)
+        self.workerModel = keras.models.clone_model(model)
     
-class EpsilonGreedyModel(DoubleQNModel):
-    def __init__(self):
-        super().__init__()
-        self.createModel()
+    def updateParameters(self):
+        pass
     
-    def createModel(self):
-        self.targetModel = load_default_model(self.input_shape, self.output_shape)
-        super().createModel()
+    def train(self):
+        pass
         
     
