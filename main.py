eb082e565b81d6dd1d151741a9d2461e9b80343c
mohammad shafiee <shafiee.mohamin@gmail.com>
2022-03-13 15:15:51 +0330

    agent implementation has been completed

--- main.py
+++ main.py
@@ -1,3 +1,62 @@
-from src.model.DoubleQNModel import EpsilonGreedyModel
+# from src.model.DoubleQNModel import EpsilonGreedyModel
+# from tensorflow import keras
+# import random
+import cv2
+from src.model.LayersWrapper import make_env, DQNAgent, load_model
+import numpy as np
+from baselines.common.atari_wrappers  import make_atari, wrap_deepmind
 
-EpsilonGreedyModel()
+
+
+
+env = make_atari("BreakoutNoFrameskip-v4")
+env = wrap_deepmind(env, frame_stack=True, scale=True)
+# env = make_env()
+# env.reset()
+n_actions = env.action_space.n
+state_dim = env.observation_space.shape
+agent = DQNAgent("ad", state_dim, n_actions)
+agent.network.load_weights("./assets/dqn_model_atari_weights.h5")
+
+model = load_model()
+model.load_weights("./_model.h5")
+
+model2 = load_model()
+model2.load_weights("./assets/model_1.h5")
+
+
+
+def test(agent, env, total_episodes=30):
+    rewards = []
+    env.seed(11037)
+    for i in range(total_episodes):
+        state = env.reset()
+        # agent.init_game_setting()
+        done = False
+        episode_reward = 0.0
+
+        #playing one game
+        while(not done):
+            # action = np.argmax(agent.network.predict(np.expand_dims(state, axis=0)))
+            # else:
+            action = np.argmax(model.predict(np.expand_dims(state, axis=0)))
+            
+            
+            new_state, reward, done, info = env.step(action)
+            episode_reward += reward
+            cv2.imshow("frames", np.array(new_state))
+            cv2.waitKey(10)
+            # print(reward)
+            state = new_state
+        
+        # env.close()
+        print("episode reward ", episode_reward)
+        rewards.append(episode_reward)
+    
+    print('Run %d episodes'%(total_episodes))
+    print('Mean:', np.mean(rewards))
+
+# # epsilonGreedy = EpsilonGreedyModel()
+# # epsilonGreedy.train(keras.optimizers.Adam(), keras.losses.Huber(), ["accuracy"])
+
+test(agent, env)
--- src/model/LayersWrapper.py
+++ src/model/LayersWrapper.py
@@ -1,6 +1,7 @@
 from tensorflow import keras
+import tensorflow as tf
 from functools import partial
-
+import numpy as np
 
 ReluDenseLayer = partial(
     keras.layers.Dense, kernel_initializer="he_normal",
@@ -34,3 +35,141 @@ def load_default_model(input_shape: list, output_units: int):
     model.summary()
     
     return model
+
+from keras.layers import Conv2D, Dense, Flatten
+class DQNAgent:
+    def __init__(self, name, state_shape, n_actions, epsilon=0, reuse=False):
+        """A simple DQN agent"""
+        # with tf.variable_scope(name, reuse=reuse):
+            
+        self.network = keras.models.Sequential()
+    
+            # Keras ignores the first dimension in the input_shape, which is the batch size. 
+            # So just use state_shape for the input shape
+        self.network.add(Conv2D(32, (8, 8), strides=4, activation='relu',use_bias=False, input_shape=state_shape,kernel_initializer=keras.initializers.variance_scaling(scale=2)))
+        self.network.add(Conv2D(64, (4, 4), strides=2, activation='relu',use_bias=False,kernel_initializer=keras.initializers.variance_scaling(scale=2)))
+        self.network.add(Conv2D(64, (3, 3), strides=1, activation='relu',use_bias=False,kernel_initializer=keras.initializers.variance_scaling(scale=2)))
+        self.network.add(Conv2D(1024, (7, 7), strides=1, activation='relu',use_bias=False,kernel_initializer=keras.initializers.variance_scaling(scale=2)))
+        self.network.add(Flatten())
+        self.network.add(Dense(n_actions, activation='linear',kernel_initializer=keras.initializers.variance_scaling(scale=2)))
+            
+            # prepare a graph for agent step
+            # self.state_t = tf.placeholder('float32', [None,] + list(state_shape))
+            # self.qvalues_t = self.get_symbolic_qvalues(self.state_t)
+            
+        # self.weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=name)
+        self.epsilon = epsilon
+
+    def get_symbolic_qvalues(self, state_t):
+        """takes agent's observation, returns qvalues. Both are tf Tensors"""
+        qvalues = self.network(state_t)
+        
+        
+        assert tf.is_numeric_tensor(qvalues) and qvalues.shape.ndims == 2, \
+            "please return 2d tf tensor of qvalues [you got %s]" % repr(qvalues)
+        assert int(qvalues.shape[1]) == n_actions
+        
+        return qvalues
+    
+    def get_qvalues(self, state_t):
+        """Same as symbolic step except it operates on numpy arrays"""
+        sess = tf.get_default_session()
+        return sess.run(self.qvalues_t, {self.state_t: state_t})
+    
+    def sample_actions(self, qvalues):
+        """pick actions given qvalues. Uses epsilon-greedy exploration strategy. """
+        epsilon = self.epsilon
+        batch_size, n_actions = qvalues.shape
+        random_actions = np.random.choice(n_actions, size=batch_size)
+        best_actions = qvalues.argmax(axis=-1)
+        should_explore = np.random.choice([0, 1], batch_size, p = [1-epsilon, epsilon])
+        return np.where(should_explore, random_actions, best_actions)
+
+from gym.core import ObservationWrapper
+from gym.spaces import Box
+
+# from scipy.misc import imresize
+import cv2
+
+class PreprocessAtari(ObservationWrapper):
+    def __init__(self, env):
+        """A gym wrapper that crops, scales image into the desired shapes and optionally grayscales it."""
+        ObservationWrapper.__init__(self,env)
+        
+        self.img_size = (84, 84)
+        self.observation_space = Box(0.0, 1.0, (self.img_size[0], self.img_size[1], 1))
+
+    def observation(self, img):
+        """what happens to each observation"""
+        
+        # crop image (top and bottom, top from 34, bottom remove last 16)
+        img = img[34:-16, :, :]
+        
+        # resize image
+        img = cv2.resize(img, self.img_size)
+        
+        img = img.mean(-1,keepdims=True)
+        
+        img = img.astype('float32') / 255.
+              
+        return img
+    
+from gym.spaces.box import Box
+from gym.core import Wrapper
+import gym
+
+class FrameBuffer(Wrapper):
+    def __init__(self, env, n_frames=4, dim_order='tensorflow'):
+        """A gym wrapper that reshapes, crops and scales image into the desired shapes"""
+        super(FrameBuffer, self).__init__(env)
+        self.dim_order = dim_order
+        if dim_order == 'tensorflow':
+            height, width, n_channels = env.observation_space.shape
+            """Multiply channels dimension by number of frames"""
+            obs_shape = [height, width, n_channels * n_frames] 
+        else:
+            raise ValueError('dim_order should be "tensorflow" or "pytorch", got {}'.format(dim_order))
+        self.observation_space = Box(0.0, 1.0, obs_shape)
+        self.framebuffer = np.zeros(obs_shape, 'float32')
+        
+    def reset(self):
+        """resets breakout, returns initial frames"""
+        self.framebuffer = np.zeros_like(self.framebuffer)
+        self.update_buffer(self.env.reset())
+        return self.framebuffer
+    
+    def step(self, action):
+        """plays breakout for 1 step, returns frame buffer"""
+        new_img, reward, done, info = self.env.step(action)
+        self.update_buffer(new_img)
+        return self.framebuffer, reward, done, info
+    
+    def update_buffer(self, img):
+        if self.dim_order == 'tensorflow':
+            offset = self.env.observation_space.shape[-1]
+            axis = -1
+            cropped_framebuffer = self.framebuffer[:,:,:-offset]
+        self.framebuffer = np.concatenate([img, cropped_framebuffer], axis = axis)
+        
+
+def make_env():
+    # env = gym.make("BreakoutDeterministic-v4")
+    env = gym.make("BreakoutDeterministic-v4")
+    env = PreprocessAtari(env)
+    env = FrameBuffer(env, n_frames=4, dim_order='tensorflow')
+    return env
+
+def load_model():
+    inputs = keras.layers.Input(shape=(84, 84, 4,))
+
+    # Convolutions on the frames on the screen
+    layer1 = keras.layers.Conv2D(32, 8, strides=4, activation="relu")(inputs)
+    layer2 = keras.layers.Conv2D(64, 4, strides=2, activation="relu")(layer1)
+    layer3 = keras.layers.Conv2D(64, 3, strides=1, activation="relu")(layer2)
+
+    layer4 = keras.layers.Flatten()(layer3)
+
+    layer5 = keras.layers.Dense(512, activation="relu")(layer4)
+    action = keras.layers.Dense(4, activation="linear")(layer5)
+
+    return keras.Model(inputs=inputs, outputs=action)
